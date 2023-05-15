3b2a68d6cd3523ebce56c77e09924ce2399abc7d
muta <vvflowervv3jsbvv@gmail.com>
2023-05-16 00:46:58 +0900

    5/15 commit home

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,39 +1,59 @@
-function Header() {
+import './Header.css'
+import './Base.css'
+import './Main.css'
+import contactLogo from'./assets/contact-logo.svg'
+import headerLogo from './assets/logo_black.png'
+
+export default function Header() {
 
     return (
         <>
-            <header>
-                <nav class="header_navigation">
-                    <a class="header_icon" href="">
-                        <img src="src\assets\logo.png" alt="HighJunk"></img>
+            <header id="header-h" class="header">
+                <nav class="header__navigation">
+                    <a class="header_icon" href="./home.html">
+                        <img src={headerLogo} alt="HighJunk"></img>
                     </a> 
-                    <div class="header_menu-button js-menu-button mod-sp">
+                    <div class="hamburger">
                         <span class="top"></span>
                         <span class="middle"></span>
                         <span class="bottom"></span>
                     </div>
-                    <ul class="header_navigation-menu-list">
-                        <li>
-                            <a class="header_navigation-menu-link parts" hraf="">
+                    <ul class="header__navigation-menu-list">
+                        <li class="header__navigation-menu-link">
+                            <a class="no-barba" hraf="">
                                 ABOUT<ruby>私たちの強み</ruby>
                             </a>
                         </li>
-                        <li class="header_navigation-menu-link">
+                        <li class="header__navigation-menu-link">
                             <span class="js=parent-link">
                                 BUSINESS<ruby>ビジネス</ruby>
                             </span>
-                            <ul class="smartphone" style="display: none;">
-                                <li><a class="parts" href="">Web&amp;APP<ruby>ウェブ&amp;アプリ開発</ruby></a></li>
-                                <li><a class="parts" href="">GAME<ruby>ゲーム開発</ruby></a></li>
-                                <li><a class="parts" href="">EC<ruby>EC開発</ruby></a></li>
-                                <li><a class="parts" href=""> UI/UX<ruby>UI/UX開発</ruby></a></li>
-                            </ul>
-                            <ul class="pc">
-                                <li><a class="parts" href="">Web&amp;APP<ruby>ウェブ&amp;アプリ開発</ruby></a></li>
-                                <li><a class="parts" href="">GAME<ruby>ゲーム開発</ruby></a></li>
-                                <li><a class="parts" href="">EC<ruby>EC開発</ruby></a></li>
-                                <li><a class="parts" href=""> UI/UX<ruby>UI/UX開発</ruby></a></li>
-                            </ul>
+                        </li>
+                        <li class="header__navigation-menu-link">
+                            <a class="no-barba" hraf="">
+                                PRODUCT<ruby>プロダクト</ruby>
+                            </a>
+                        </li>
+                        <li class="header__navigation-menu-link">
+                            <a class="no-barba" hraf="">
+                                WORKS<ruby>実績</ruby>
+                            </a>
+                        </li>
+                        <li class="header__navigation-menu-link">
+                            <span class="js=parent-link">
+                                COMPANY<ruby>企業情報</ruby>
+                            </span>
+                        </li>
+                        <li class="header__navigation-menu-link">
+                            <a class="no-barba" hraf="">
+                                RECRUIT<ruby>リクルート</ruby>
+                            </a>
+                        </li>
+                        <li class="header__navigation-menu-link">
+                            <a class="no-barba" hraf="">
+                                <img src={contactLogo} alt="contact-logo"></img>
+                                <ruby>お問い合わせ</ruby>
+                            </a>
                         </li>
                     </ul> 
                 </nav>
